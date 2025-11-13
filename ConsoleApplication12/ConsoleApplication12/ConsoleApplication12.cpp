a49d156b709ff7397825988d1ec47bb1b6315308
werzic <sonyasemen02@gmail.com>
2025-11-13 15:54:51 +0300

    Add files via upload

---
+++ ConsoleApplication12/ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		setlocale(0, "");
+
+		int n;
+		int i = 1;
+		cin >> n;
+		while (i < n ) {
+			cout << "Цикл отработал. Круг:" << i << endl;
+			i = i + 1;
+		}
+
+    return 0;
+}
+
+
